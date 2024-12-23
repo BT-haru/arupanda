7dedf753f72a62d04c5c3121227a2962fb39d336
satomi0219 <mizusatom0219@gmail.com>
2024-12-23 16:15:16 +0900

    ai.py

--- ai.py
+++ ai.py
@@ -108,7 +108,7 @@ def simulate_move(board, stone, x, y):
 class PandaAI(object):
 
     def face(self):
-        return "🐼1"
+        return "🐇"
 
     def place(self, board, stone):
         x, y = random_place(board, stone)
